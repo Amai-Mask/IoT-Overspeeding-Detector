ffdd7317e15807af8ca5052286e5928d2ca34810
Munadim Ahsan Uosmoy <50762267+Amai-Mask@users.noreply.github.com>
2024-05-05 20:12:05 +0600

    update code 001

--- OverSpeed Detector.ino
+++ OverSpeed Detector.ino
@@ -1,92 +1,94 @@
 #include <NewPing.h>
-#include <ThingSpeak.h> // Include the ThingSpeak library
+#include <ThingSpeak.h>
 #include <ESP8266WiFi.h>
-#include <LiquidCrystal.h> // Include the LiquidCrystal library
+#include <LiquidCrystal.h>
 #include <WiFiClientSecure.h> 
 #include <UniversalTelegramBot.h>
 
 #define trigPin D6
 #define echoPin D5
+#define FOR_LIGHT 50
+#define buzzerPin D7 
 #define redLedPin D7
 #define greenLedPin D6
-#define buzzerPin D7 // Define buzzer pin
-#define SOUND_SPEED 34300 // Speed of sound in cm/s at 20 degrees Celsius
-#define DISTANCE_THRESHOLD 500 // Threshold distance in centimeters to trigger speed calculation (adjust as needed)
-#define MIN_SPEED_THRESHOLD 10 // Minimum speed threshold in cm/s
-#define FOR_LIGHT 50 // Minimum speed threshold in cm/s
-#define MIN_DISTANCE_THRESHOLD 500 // Minimum distance threshold in centimeters to trigger red LED
+#define SOUND_SPEED 34300 
+#define MIN_SPEED_THRESHOLD 10
+#define DISTANCE_THRESHOLD 500
+#define MIN_DISTANCE_THRESHOLD 500 
 
-NewPing sonar(trigPin, echoPin);
+// ThingSpeak Channel
+unsigned long channelID = 2526286;
+const char *writeAPIKey = "W7CXCNXXADZ8MSL5";
 
-// ThingSpeak Channel Settings
-unsigned long channelID = 2526286; // Replace with your ThingSpeak channel ID
-const char *writeAPIKey = "W7CXCNXXADZ8MSL5"; // Replace with your ThingSpeak write API key
+// WiFi
+const char* ssid = "Amai Mask";
+const char* password = "01554333427";
 
-const char* ssid = "Amai Mask";     // Your WiFi SSID
-const char* password = "01554333427"; // Your WiFi password
+//Telegram Bot
+#define CHAT_ID "6960041740" 
+#define BOTtoken "7181653196:AAEl8JBZKPTkIWCl1MoEuS3ldaenYjillNw"
 
-// const char *host = "api.telegram.org";
-// const int httpsPort = 443;
-// const char *botToken = "7181653196:AAEl8JBZKPTkIWCl1MoEuS3ldaenYjillNw";
-// const char *chatID = "7181653196";
-WiFiClient client; // Create a WiFi client object
-
-#define BOTtoken "7181653196:AAEl8JBZKPTkIWCl1MoEuS3ldaenYjillNw" // Enter the bottoken you got from botfather
-#define CHAT_ID "6960041740" // Enter your chatID you got from chatid bot
-
-
-X509List cert(TELEGRAM_CERTIFICATE_ROOT);
+WiFiClient client;
 WiFiClientSecure clnt;
+NewPing sonar(trigPin, echoPin);
+X509List cert(TELEGRAM_CERTIFICATE_ROOT);
 UniversalTelegramBot bot(BOTtoken, clnt);
+LiquidCrystal lcd(D0, D1, D2, D3, D4, D8); 
 
-
-LiquidCrystal lcd(D0, D1, D2, D3, D4, D8); // Define LCD pins
-
-unsigned long previousTime = 0;
 float previousDistance = 0;
+unsigned long previousTime = 0;
 
-void setup() {
+void setup() 
+{
   Serial.begin(9600);
-  lcd.begin(16, 2); // Initialize the LCD with 16 columns and 2 rows
+
+  lcd.begin(16, 2);
+
   configTime(0, 0, "pool.ntp.org");      
   clnt.setTrustAnchors(&cert);
+
   pinMode(redLedPin, OUTPUT);
-  pinMode(greenLedPin, OUTPUT);  // Set red LED pin as output
-  pinMode(buzzerPin, OUTPUT); // Set buzzer pin as output
+  pinMode(buzzerPin, OUTPUT);
+  pinMode(greenLedPin, OUTPUT);
+
   digitalWrite(redLedPin, LOW);
-  digitalWrite(greenLedPin, HIGH); // Ensure red LED is initially off
-  digitalWrite(buzzerPin, LOW); // Ensure buzzer is initially off
-  // Connect to WiFi
+  digitalWrite(buzzerPin, LOW);
+  digitalWrite(greenLedPin, HIGH);
+
   Serial.print("Connecting to WiFi");
   WiFi.mode(WIFI_STA);
   WiFi.begin(ssid, password);
-  while (WiFi.status() != WL_CONNECTED) {
+  while (WiFi.status() != WL_CONNECTED) 
+  {
     delay(50);
     Serial.print(".");
   }
   Serial.println("WiFi connected");
   bot.sendMessage(CHAT_ID, "System has Started!!", "");
-  ThingSpeak.begin(client); // Initialize ThingSpeak client with the WiFi client
+  ThingSpeak.begin(client);
   delay(50);
 }
 
-void loop() {
+void loop() 
+{
   unsigned long currentTime = millis();
-  float distance = sonar.ping_cm(); // Distance in centimeters
+  float distance = sonar.ping_cm();
 
-  if (distance < DISTANCE_THRESHOLD && distance > 0) {
-    if (previousDistance == 0) {
-      // First measurement
+  if (distance < DISTANCE_THRESHOLD && distance > 0) 
+  {
+    if (previousDistance == 0) 
+    {
       previousDistance = distance;
       previousTime = currentTime;
-    } else {
-      // Calculate speed
+    } 
+    else 
+    {
       unsigned long elapsedTime = currentTime - previousTime;
-      float speed = abs(previousDistance - distance) / (float)elapsedTime; // Speed in cm/s
-      speed *= 2000; // Convert speed to more readable units
-      // Check if speed exceeds minimum threshold
-      if (speed > MIN_SPEED_THRESHOLD) {
-        // Print overspeed and speed to LCD
+      float speed = abs(previousDistance - distance) / (float)elapsedTime;
+      speed *= 2000;
+
+      if (speed > MIN_SPEED_THRESHOLD) 
+      {
         Serial.print("Distance: ");
         Serial.print(distance);
         Serial.println(" cm");
@@ -95,14 +97,13 @@ void loop() {
         Serial.println(" cm/s");
       }
 
-      // Check if distance is less than minimum threshold and speed is greater than or equal to the threshold for turning on the light
-      if (distance < MIN_DISTANCE_THRESHOLD && speed >= FOR_LIGHT) {
-        // Turn on red LED
+      if (distance < MIN_DISTANCE_THRESHOLD && speed >= FOR_LIGHT) 
+      {
         digitalWrite(redLedPin, HIGH);
-        // Turn on buzzer
+        digitalWrite(buzzerPin, HIGH);
         digitalWrite(greenLedPin, LOW);
         delay(100);
-        digitalWrite(buzzerPin, HIGH);
+
         lcd.clear();
         lcd.setCursor(0, 0);
         lcd.print("Overspeed:");
@@ -110,33 +111,29 @@ void loop() {
         lcd.print(speed);
         lcd.print(" cm/s");
         bot.sendMessage(CHAT_ID, "Red light is on!\nSpeed: " + String(speed) + " cm/s\nDistance: " + String(distance) + " cm", "");
-
-
-      } else {
-        // Turn off red LED
+      } 
+      else 
+      {
         digitalWrite(redLedPin, LOW);
-        // Turn off buzzer
-        digitalWrite(greenLedPin, HIGH);
         digitalWrite(buzzerPin, LOW);
+        digitalWrite(greenLedPin, HIGH);
       }
       
-      // Send speed and distance data to ThingSpeak
       ThingSpeak.writeField(channelID, 1, distance, writeAPIKey);
       ThingSpeak.writeField(channelID, 2, speed, writeAPIKey);
 
-      // Update previous values
       previousDistance = distance;
       previousTime = currentTime;
     }
-  } else {
-    // Reset previous values if the car is too far away or if the distance is invalid
+  } 
+  else 
+  {
+    lcd.clear();
     previousDistance = 0;
-    digitalWrite(redLedPin, LOW); // Turn off red LED if the car is too far away
+    digitalWrite(redLedPin, LOW); 
+    digitalWrite(buzzerPin, LOW);
     digitalWrite(greenLedPin, HIGH);
-    digitalWrite(buzzerPin, LOW); // Turn off buzzer if the car is too far away
-    lcd.clear(); // Clear LCD display
   }
-
-  delay(10); // Adjust as needed depending on the expected speed of the car
+  delay(10);
 }
 
